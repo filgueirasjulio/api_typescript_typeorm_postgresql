1d1d781d2d15d01710ca231c32a02c3c4d9af999
filgueirasjulio <julio_filgueiras@live.com>
2023-02-21 18:39:37 -0300

    express configurado

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('started')
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const PORT = process.env.PORT || 8080
+
+const app = express()
+app.get('/', (request, response) => {
+  response.send("server up")
+})
+app.listen(PORT, () => {
+  console.log(`server runing in port ${PORT}`)
+})
